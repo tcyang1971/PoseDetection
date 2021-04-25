32193d94fa60befebdd197e4edd5dc3b7c357147
tcyang <tcyang@gm.pu.edu.tw>
2021-04-25 13:48:29 +0800

    初始專案

---
+++ app/src/main/java/tw/edu/pu/csim/tcyang/posedetection/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.csim.tcyang.posedetection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
