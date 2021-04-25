939b709e08bcd20b1ad0d45972e9e0f60c432bb2
tcyang <tcyang@gm.pu.edu.tw>
2021-04-25 15:53:42 +0800

    create an instance of PoseDetector

--- app/src/main/java/tw/edu/pu/csim/tcyang/posedetection/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/posedetection/MainActivity.kt
@@ -4,6 +4,9 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +20,19 @@ class MainActivity : AppCompatActivity() {
         //將drawable的圖片轉換成Bitmap
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.miss)
         img.setImageBitmap(bitmap)
+
+        /*
+        // Base pose detector with streaming frames, when depending on the pose-detection sdk
+        val options = PoseDetectorOptions.Builder()
+            .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
+            .build()
+         */
+
+        // Accurate pose detector on static images, when depending on the pose-detection-accurate sdk
+        val options = AccuratePoseDetectorOptions.Builder()
+            .setDetectorMode(AccuratePoseDetectorOptions.SINGLE_IMAGE_MODE)
+            .build()
+        val poseDetector = PoseDetection.getClient(options)
 
     }
 }
