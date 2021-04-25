b75bf4aab896b0ab8228be203dbe8fc0ebe80ae1
tcyang <tcyang@gm.pu.edu.tw>
2021-04-25 14:05:57 +0800

    顯示Bitmap點陣圖

--- app/src/main/java/tw/edu/pu/csim/tcyang/posedetection/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/posedetection/MainActivity.kt
@@ -1,11 +1,22 @@
 package tw.edu.pu.csim.tcyang.posedetection
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bitmap: Bitmap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //將drawable的圖片轉換成Bitmap
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.miss)
+        img.setImageBitmap(bitmap)
+
     }
 }
